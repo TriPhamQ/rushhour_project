f1700eea78a5d29c63f6f8edbf94fa434a58d9c5
maxwell <m.nelson.study@gmail.com>
2017-02-01 14:57:33 -0800

    graph

--- client/static/assets/controllers/graphController.js
+++ client/static/assets/controllers/graphController.js
@@ -1,6 +1,6 @@
 myApp.controller('graphController', ['$scope', '$location', '$rootScope', '$cookies', 'businessFactory',  function ($scope, $location, $rootScope, $cookies, businessFactory){
 	  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-	  $scope.series = ['Series A', 'Series B'];
+	  $scope.series = ['Series A'];
 	  $scope.data = [
 	    [65, 59, 80, 81, 56, 55, 40],
 	    [28, 48, 40, 19, 86, 27, 90]
