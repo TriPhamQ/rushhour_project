c2908a6bbc4c1ae9fb96119af5575c5f680a71c1
Tri Pham <phamqtri1992@gmail.com>
2017-02-01 18:29:30 -0800

    Ways to load times by sec hour minute

--- client/static/assets/controllers/graphController.js
+++ client/static/assets/controllers/graphController.js
@@ -1,28 +1,35 @@
 myApp.controller('graphController', ['$scope', '$location', '$rootScope', '$cookies', 'graphFactory',  function ($scope, $location, $rootScope, $cookies, graphFactory){
 	$scope.labels = [];
-	$scope.series = ['Series A'];
+	$scope.series = [];
 	$scope.data = [[]];
 
 	graphFactory.getData(function(output){
 		console.log('output');
 		var len = output.data.length;
+		console.log(output.data);
 		for (var i = 0; i < len; i++){
-			var leng = output.data[i].count_time.length
-			console.log(output.data[i]);
+			var leng = output.data[i].count_time.length;
+			$scope.series.push(output.data[i]._id);
+			var timez = new Date(output.data[i].createdAt);
+			console.log("Seconds",timez.getSeconds());
+			console.log("Hours",timez.getHours());
+			console.log("Minutes",timez.getMinutes());
+			console.log(output.data[i].createdAt);
 			for (var j = 0; j < leng; j++){
 				$scope.labels.push(output.data[i].count_time[j]);
-				$scope.data[0].push(j);
+				$scope.data[0].push(j+1);
 			}
 		}
-		console.log('labels');
-		console.log($scope.labels);
+		// console.log('labels');
+		// console.log($scope.labels);
+		// console.log($scope.series);
 	})
 
 
 
 
 	$scope.GraphClick = function (points, evt) {
-	console.log(points, evt);
+	// console.log(points, evt);
 	};
 	$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
 	$scope.options = {
@@ -33,7 +40,7 @@ myApp.controller('graphController', ['$scope', '$location', '$rootScope', '$cook
 	      type: 'linear',
 	      display: true,
 	      position: 'left'
-	    },
+	    }
 	  ]
 	}
 	};
