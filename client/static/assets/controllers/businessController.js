7be4a15c4262018f224a1712a6ff5a1c2bb0a43b
Tri Pham <phamqtri1992@gmail.com>
2017-02-01 21:07:53 -0800

    Switched graphcontroller to use rootScope for real time update when add count or delete. The graph is not yet serve the correct purpose tho.

--- client/static/assets/controllers/businessController.js
+++ client/static/assets/controllers/businessController.js
@@ -1,4 +1,4 @@
-myApp.controller('businessController', ['$scope', '$location', '$rootScope', '$cookies', 'businessFactory',  function ($scope, $location, $rootScope, $cookies, businessFactory){
+myApp.controller('businessController', ['$scope', '$location', '$rootScope', '$cookies', 'businessFactory', 'graphFactory', function ($scope, $location, $rootScope, $cookies, businessFactory, graphFactory){
 
 	console.log('businessController');
 
@@ -92,6 +92,32 @@ myApp.controller('businessController', ['$scope', '$location', '$rootScope', '$c
 		    	$scope.items = output;
 		    	console.log($scope.items);
 			});
+			// CHANGE THIS PART AFTER FINALIZED THE GRAPH~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			graphFactory.getData(function(output){
+				$rootScope.labels = [];
+				$rootScope.series = [];
+				$rootScope.data = [[]];
+				console.log('output');
+				var len = output.data.length;
+				console.log(output.data);
+				for (var i = 0; i < len; i++){
+					var leng = output.data[i].count_time.length;
+					$rootScope.series.push(output.data[i]._id);
+					var timez = new Date(output.data[i].createdAt);
+					console.log("Seconds",timez.getSeconds());
+					console.log("Hours",timez.getHours());
+					console.log("Minutes",timez.getMinutes());
+					console.log(output.data[i].createdAt);
+					for (var j = 0; j < leng; j++){
+						$rootScope.labels.push(output.data[i].count_time[j]);
+						$rootScope.data[0].push(j+1);
+					};
+				};
+				// console.log('labels');
+				// console.log($scope.labels);
+				// console.log($scope.series);
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		});
 		});
 	};
 
@@ -101,6 +127,32 @@ myApp.controller('businessController', ['$scope', '$location', '$rootScope', '$c
 		businessFactory.deleteItem(id, function(output) {
 			console.log(output);
 			$scope.items = output.data;
+			// CHANGE THIS PART AFTER FINALIZED THE GRAPH~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			graphFactory.getData(function(output){
+				$rootScope.labels = [];
+				$rootScope.series = [];
+				$rootScope.data = [[]];
+				console.log('output');
+				var len = output.data.length;
+				console.log(output.data);
+				for (var i = 0; i < len; i++){
+					var leng = output.data[i].count_time.length;
+					$rootScope.series.push(output.data[i]._id);
+					var timez = new Date(output.data[i].createdAt);
+					console.log("Seconds",timez.getSeconds());
+					console.log("Hours",timez.getHours());
+					console.log("Minutes",timez.getMinutes());
+					console.log(output.data[i].createdAt);
+					for (var j = 0; j < leng; j++){
+						$rootScope.labels.push(output.data[i].count_time[j]);
+						$rootScope.data[0].push(j+1);
+					};
+				};
+				// console.log('labels');
+				// console.log($scope.labels);
+				// console.log($scope.series);
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		});
 		});
 	}
 }]);
--- client/static/assets/controllers/graphController.js
+++ client/static/assets/controllers/graphController.js
@@ -1,31 +1,32 @@
 myApp.controller('graphController', ['$scope', '$location', '$rootScope', '$cookies', 'graphFactory',  function ($scope, $location, $rootScope, $cookies, graphFactory){
-	$scope.labels = [];
-	$scope.series = [];
-	$scope.data = [[]];
+	$rootScope.labels = [];
+	$rootScope.series = [];
+	$rootScope.data = [[]];
 
-	graphFactory.getData(function(output){
-		console.log('output');
-		var len = output.data.length;
-		console.log(output.data);
-		for (var i = 0; i < len; i++){
-			var leng = output.data[i].count_time.length;
-			$scope.series.push(output.data[i]._id);
-			var timez = new Date(output.data[i].createdAt);
-			console.log("Seconds",timez.getSeconds());
-			console.log("Hours",timez.getHours());
-			console.log("Minutes",timez.getMinutes());
-			console.log(output.data[i].createdAt);
-			for (var j = 0; j < leng; j++){
-				$scope.labels.push(output.data[i].count_time[j]);
-				$scope.data[0].push(j+1);
+	$scope.getdata = function () {
+		graphFactory.getData(function(output){
+			console.log('output');
+			var len = output.data.length;
+			console.log(output.data);
+			for (var i = 0; i < len; i++){
+				var leng = output.data[i].count_time.length;
+				$rootScope.series.push(output.data[i]._id);
+				var timez = new Date(output.data[i].createdAt);
+				console.log("Seconds",timez.getSeconds());
+				console.log("Hours",timez.getHours());
+				console.log("Minutes",timez.getMinutes());
+				console.log(output.data[i].createdAt);
+				for (var j = 0; j < leng; j++){
+					$rootScope.labels.push(output.data[i].count_time[j]);
+					$rootScope.data[0].push(j+1);
+				}
 			}
-		}
-		// console.log('labels');
-		// console.log($scope.labels);
-		// console.log($scope.series);
-	})
-
-
+			// console.log('labels');
+			// console.log($scope.labels);
+			// console.log($scope.series);
+		})
+	};
+	$scope.getdata();
 
 
 	$scope.GraphClick = function (points, evt) {
