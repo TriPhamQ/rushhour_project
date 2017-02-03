08fdbcf2cb672af5be6590ce9619ed7af8b58321
Tri Pham <phamqtri1992@gmail.com>
2017-02-02 23:28:15 -0800

    Revert "Revert "update markers is now a standalone function, so when markers are updated the entire map does not need to be reloaded.""
    This reverts commit 0e4f4e2a9ae01b7a7ddcdccb2592b91e332cd3d5.

--- client/static/assets/factories/mapFactory.js
+++ client/static/assets/factories/mapFactory.js
@@ -3,6 +3,8 @@ myApp.factory('mapFactory', ['$http', function ($http) {
 
 	factory = {};
 
+	var map;
+
 	var locations = [];
 	var busiest = [];
 	var selectedLat = null;
@@ -15,6 +17,7 @@ myApp.factory('mapFactory', ['$http', function ($http) {
 		selectedLong = long;
 
 		$http.get('/markers').then(function(output){
+			console.log('happening');
 			locations = convertToMapPoints(output.data);
 			initialize(lat, long);
 		});
@@ -50,13 +53,29 @@ myApp.factory('mapFactory', ['$http', function ($http) {
 		var myLatLng = {lat: selectedLat, lng: selectedLong};
 
 		if (!map){
-			var map = new google.maps.Map(document.getElementById('map'), {
+			map = new google.maps.Map(document.getElementById('map'), {
 				zoom: 1,
 				center: myLatLng
 			});
 		}
 
-		locations.forEach(function(n, i){
+		updateMarkers();
+
+		var initalLocation = new google.maps.LatLng(lat, long);
+		var marker = new google.maps.Marker({
+			position: initalLocation,
+			animation: google.maps.Animation.Bounce,
+			map: map,
+			icon: ''
+		});
+		lastMarker = marker;
+		// Place a draggable marker on the map
+
+
+	};
+
+	var updateMarkers = function(){
+				locations.forEach(function(n, i){
 			console.log('N is : ', n.username);
 			var flag = 0;
 			for (var cat = 0; cat < busiest.length; cat++){
@@ -87,26 +106,14 @@ myApp.factory('mapFactory', ['$http', function ($http) {
 				n.message.open(map, marker);
 			});
 		});
-
-		var initalLocation = new google.maps.LatLng(lat, long);
-		var marker = new google.maps.Marker({
-			position: initalLocation,
-			animation: google.maps.Animation.Bounce,
-			map: map,
-			icon: ''
-		});
-		lastMarker = marker;
-		// Place a draggable marker on the map
-
-
-	};
+	}
 
 
 	var socket = io.connect();
     socket.on('mapUp', function(data){
         console.log('these are the locations', data.data.local);
         busiest.push(data.data.local);
-        initialize(selectedLat, selectedLong);
+        updateMarkers();
     })
     socket.on('mapDown', function(data){
         console.log('locations not busy', data);
@@ -118,7 +125,7 @@ myApp.factory('mapFactory', ['$http', function ($http) {
         	}
         }
         busiest.splice(x, 1);
-        initialize(selectedLat, selectedLong);
+        updateMarkers();
     })
 
 	// google.maps.event.addDomListener(window, 'load',
